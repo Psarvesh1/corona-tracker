7dd99b6aae2ad9041f2f56336eb1e4fc661e5c4b
Psarvesh1 <sarveshparab602@gmail.com>
2020-04-24 22:45:44 +0530

    Initial commit

---
+++ precache-manifest.e768a3d2bd056429549260fd78969641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f3f6a50e935133dcd00f7319df5354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c32b587fc2e64a5628",
+    "url": "/static/css/main.a9271d06.chunk.css"
+  },
+  {
+    "revision": "8f7df7e647d1733e7e18",
+    "url": "/static/js/2.e861a70e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.e861a70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c32b587fc2e64a5628",
+    "url": "/static/js/main.3106d3ba.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
